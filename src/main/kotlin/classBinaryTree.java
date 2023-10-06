9200282bb3399d6f0cdb5f40a43058b1af7936a0
nighttea <mar.p.and32@gmail.com>
2023-10-06 20:03:29 +0300

    Add adding node

--- src/main/kotlin/classBinaryTree.java
+++ src/main/kotlin/classBinaryTree.java
@@ -4,18 +4,71 @@ public class classBinaryTree {
         int value;
         Node leftChild;
         Node rightChild;
-
+        Node previous;
         Node(int value)
         {
             this.value = value;
-            leftChild = null;
-            rightChild = null;
+            /*leftChild = null;
+            rightChild = null;*/
         }
     }
 
-    Node root;
+    Node root = null;
 
-    public void addNode(int value) {
+   /* public void addNode(int value) {
+        root = new Node(value);
+        addNode(value, root);
+    }*/
+
+    public void addNode(int value)
+    {
+        if(root == null)
+        {
+            root = new Node(value);
+            root.leftChild = null;
+            root.rightChild = null;
+            root.previous = null;
+
+        }
+        else
+        {
+            Node currentNode = root;
+            while (currentNode != null)
+            {
+                if(value < currentNode.value)
+                {
+                    if(currentNode.leftChild == null)
+                    {
+                        Node newNode = new Node(value);
+                        newNode.leftChild = null;
+                        newNode.rightChild = null;
+                        newNode.previous = currentNode;
+                        currentNode.leftChild = newNode;
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.leftChild;
+                    }
+                }
+                else
+                {
+                    if(currentNode.rightChild == null)
+                    {
+                        Node newNode = new Node(value);
+                        newNode.leftChild = null;
+                        newNode.rightChild = null;
+                        newNode.previous = currentNode;
+                        currentNode.rightChild = newNode;
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.rightChild;
+                    }
+                }
+            }
+        }
 
     }
 }
